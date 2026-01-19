36814eda949c228d7dbcb10f364f181283c6cbab
ayon2652 <ayoub199@gmail.com>
2026-01-19 01:37:28 +0000

    static files fixed

--- autolux/settings.py
+++ autolux/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +30,12 @@ SECRET_KEY = 'django-insecure-(^@r_#!8ihz^4n$hn^$%o6kr!p9qr$hb2gjl29m8qxs6cskbt4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["nas.ayoubetmaiti.com", "127.0.0.1"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+
+
+STATIC_ROOT = "/var/www/autolux_static"
 
 
 
